e2b0a9e575824ce29d5d8a5380a8411fd43ba850
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 19:20:11 +0100

    creating ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+    var myRes=75;
+
+    
+        //Only change code below this line
+
+       myRes=++myRes;
+       
+
+      
+
+        //Only change code below this line
+   
+
+    return myRes;
+}
+
+console.log(main());  
+
+module.exports=main;
