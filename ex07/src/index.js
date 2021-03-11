b8de36ba04994a446157ae3557931b904319d803
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 20:29:13 +0100

    creating ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne,numTwo){
+   
+    
+    //Only change code below this line
+
+   remainder=numOne%numTwo;
+
+  
+
+    //Only change code below this line
+
+
+return remainder;
+}
+
+//console.log(main(numOne,numTwo)); //Change this line  
+console.log(main(5,2));
+console.log(main(15,4));
+console.log(main(102,25));
+
+module.exports=main;
