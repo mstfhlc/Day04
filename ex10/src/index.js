d649c79851b962bd780e45a4127840abbcc01dad
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 20:46:12 +0100

    creating ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,26 @@
+function main(){
+   
+    
+    //Only change code below this line
+
+    myFirstName="MUSTAFA";
+    myLastName="HALICI";
+    myHomeTown="SARAJEVO";
+
+  
+
+    //Only change code below this line
+
+
+return {
+   myFirstName,
+   myLastName,
+   myHomeTown
+
+};
+}
+
+console.log(main()); 
+
+
+module.exports=main;
