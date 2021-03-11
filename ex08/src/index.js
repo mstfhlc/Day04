2a727ec2e103ee9c2d080c8d2898884e9b4e0455
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 20:36:27 +0100

    creating ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,29 @@
+function main(){
+   
+    
+    //Only change code below this line
+
+   myX=12;
+   myY=19;
+   myZ=24;
+
+   myX+=3;
+   myY+=17;
+   myZ-=20;
+
+  
+
+    //Only change code below this line
+
+
+return {
+    myX,
+    myY,
+    myZ
+};
+}
+
+console.log(main()); 
+
+
+module.exports=main;
