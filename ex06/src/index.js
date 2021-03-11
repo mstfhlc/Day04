60ba4147b1dc2dbcc3740fb0a771e26ce8e3a92a
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 19:43:09 +0100

    creating ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne,numTwo){
+   
+    
+    //Only change code below this line
+
+   myQuotient=numOne/numTwo;
+
+  
+
+    //Only change code below this line
+
+
+return myQuotient;
+}
+
+//console.log(main(numOne,numTwo)); //Change this line  
+console.log(main(5.0,2.0));
+console.log(main(16.5,5.5));
+console.log(main(102.0,25.5));
+
+module.exports=main;
