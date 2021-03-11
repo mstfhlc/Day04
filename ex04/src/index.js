4ef90b2342c73d5e42a9df537f3eab75a446fe9b
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 19:31:27 +0100

    creating ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,23 @@
+function main(){
+   
+    
+        //Only change code below this line
+
+        myDecimal=8.2;
+        myDecimalE=0.00254;
+       
+
+      
+
+        //Only change code below this line
+   
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());  
+
+module.exports=main;
