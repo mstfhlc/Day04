3aea53b99e8bda5c7591e483613d77278c4d873d
mstfhlc <mustafahalici30@gmail.com>
2021-03-11 19:23:07 +0100

    creating ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+    var myRes=16;
+
+    
+        //Only change code below this line
+
+       myRes=--myRes;
+       
+
+      
+
+        //Only change code below this line
+   
+
+    return myRes;
+}
+
+console.log(main());  
+
+module.exports=main;
